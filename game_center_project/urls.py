513b311f32099e25538d13e92cad480d1c86f8bf
max <max003003003@gmail.com>
2014-10-20 11:19:27 +0700

    Initial commit

---
+++ game_center_project/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from game_center import views
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'game_center_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',views.index),
+)
