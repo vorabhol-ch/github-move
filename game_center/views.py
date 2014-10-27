20e52a3bcaa1eabe07d64ce0bae334155988c2d8
max003003003 <max003003003@hotmail.com>
2014-10-28 01:51:16 +0700

    About me

--- game_center/views.py
+++ game_center/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     context_dict = {'boldmessage': "Game Center Champion"}
     return render(request,'game_center/index.html', context_dict)
+
+def about(request):
+    return render(request,'game_center/about.html')
